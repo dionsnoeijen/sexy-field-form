6f7ff780e4081322196a93402415ae0379647e26
Dion Snoeijen <hallo@dionsnoeijen.nl>
2017-10-02 11:03:53 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/dionsnoeijen/section-field.svg?branch=master)](https://travis-ci.org/dionsnoeijen/section-field)
+
+# SectionField Form
+
---
+++ src/Twig/SectionFormTwigExtension.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the SexyField package.
+ *
+ * (c) Dion Snoeijen <hallo@dionsnoeijen.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types=1);
+
+namespace Tardigrades\Twig;
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Tardigrades\SectionField\Form\FormInterface;
+use Tardigrades\SectionField\Service\CreateSectionInterface;
+use Tardigrades\SectionField\Service\SectionManagerInterface;
+use Tardigrades\SectionField\ValueObject\SectionFormOptions;
+use Twig_Extension;
+use Twig_Function;
+
+class SectionFormTwigExtension extends Twig_Extension
+{
+    /** @var SectionManagerInterface */
+    private $sectionManager;
+
+    /** @var FormInterface */
+    private $form;
+
+    /** @var CreateSectionInterface */
+    private $createSection;
+
+    /** @var RequestStack */
+    private $requestStack;
+
+    public function __construct(
+        SectionManagerInterface $sectionManager,
+        CreateSectionInterface $createSection,
+        FormInterface $form,
+        RequestStack $requestStack
+    ) {
+        $this->sectionManager = $sectionManager;
+        $this->createSection = $createSection;
+        $this->form = $form;
+        $this->requestStack = $requestStack;
+    }
+
+    public function getFunctions(): array
+    {
+        return array(
+            new Twig_Function(
+                'sectionForm',
+                array($this, 'sectionForm')
+            ),
+            new Twig_Function(
+                'formJavascript',
+                array($this, 'formJavascript')
+            )
+        );
+    }
+
+    public function sectionForm(
+        string $forHandle,
+        array $sectionFormOptions = []
+    ): FormView {
+
+        $sectionFormOptions = SectionFormOptions::fromArray($sectionFormOptions);
+
+        $form = $this->form->buildFormForSection(
+            $forHandle,
+            $sectionFormOptions
+        );
+        $form->handleRequest();
+
+        if ($form->isSubmitted() &&
+            $form->isValid()
+        ) {
+            $data = $form->getData();
+            $request = $this->requestStack->getCurrentRequest();
+
+            $relationships = $this->form->hasRelationship($request->get('form'));
+            $this->createSection->save($data, $relationships);
+
+            try {
+                $redirect = $sectionFormOptions->getRedirect();
+            } catch (\Exception $exception) {
+                $redirect = '/';
+            }
+            header('Location: ' . $redirect);
+            exit;
+        }
+
+        return $form->createView();
+    }
+
+    /**
+     * A form can have javascript powered fields,
+     * fetch them to bring them to your template
+     */
+    public function formJavascript()
+    {
+
+    }
+}
